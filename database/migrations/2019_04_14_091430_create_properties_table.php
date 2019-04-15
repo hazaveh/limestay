072681afc768e3a982f267b396dab8efb3c41120
Mahdi Hazaveh <mahdi@hazaveh.net>
2019-04-15 16:27:52 +0800

    migrations for properties & bookings table

---
+++ database/migrations/2019_04_14_091430_create_properties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('property_name');
+            $table->string('property_id')->unique();
+            $table->text('address')->nullable();
+            $table->text('href');
+            $table->text('response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
